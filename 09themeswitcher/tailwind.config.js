47554b2940d2967fd138521c7f68fff297fa4937
Md Noor Ali Sarwar <mdnoorali2003@gmail.com>
2026-02-10 16:03:22 +0530

    tailwind config added to contextNotes

---
+++ 09themeswitcher/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      // yahan future me custom colors, fonts, spacing add kar sakte ho
+    },
+  },
+  plugins: [],
+}
